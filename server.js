8124b76a4d915176d7ee669196bb0ea3d1520af0
fachririzki1709-arch <fachririzki1709@gmail.com>
2026-07-27 11:54:20 +0700

    server.js

--- server.js
+++ server.js
@@ -15,12 +15,17 @@ const rateLimit = require('express-rate-limit');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
+app.set('trust proxy', 1); 
 
-// --- MIDDLEWARE ---
+//  MIDDLEWARE ---
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors());
 
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // Rate Limiting
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, 
